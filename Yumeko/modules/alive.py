b6796918fb0dd7d67bfd60749a7c101ab3280bd8
DegenerateUSER <89341570+DegenerateUSER@users.noreply.github.com>
2022-02-03 23:42:32 +0530

    Update alive.py

--- Yumeko/modules/alive.py
+++ Yumeko/modules/alive.py
@@ -12,18 +12,18 @@ PHOTO = "https://images3.alphacoders.com/115/1156277.png"
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  YUMEKO = "**Holla I'm Izumi!** \n\n"
+  YUMEKO = "**Holla I'm Himeno!** \n\n"
   YUMEKO += "×**I'm Working Properly** \n\n"
   YUMEKO += "×**My Owner : [Zeref](https://t.me/FringesBright)**× \n\n"
   YUMEKO += f"×**Telethon Version : {tlhver}** \n\n"
   YUMEKO += f"×**Pyrogram Version : {pyrover}** \n\n"
   YUMEKO += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Izumigroupbot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/izumibotsupport")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/HimenoXBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/Himenosupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=YUMEKO,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
 async def reload(event):
   tai = event.sender.first_name
   YUMEKO = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
-  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/izumibotsupport")]]
+  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/Himenosupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=YUMEKO,  buttons=BUTTON)
