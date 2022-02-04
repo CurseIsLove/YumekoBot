5fab62f62ef7f76c9db86b13ad9267610c17ccaa
Anshul Garg <garganshul553@gmail.com>
2022-02-04 21:03:11 +0530

    Update __main__.py

--- Yumeko/__main__.py
+++ Yumeko/__main__.py
@@ -329,6 +329,21 @@ def help_button(update, context):
                     paginate_modules(0, HELPABLE, "help")
                 ),
             )
+                                     
+        elif query.data == "asuna_back":
+            first_name = update.effective_user.first_name
+            query.message.edit_text(
+                    PM_START_TEXT.format(
+                        escape_markdown(context.bot.first_name),
+                        escape_markdown(first_name),
+                        escape_markdown(uptime),
+                        sql.num_users(),
+                        sql.num_chats()),
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                    parse_mode=ParseMode.MARKDOWN,
+                    timeout=60,
+                    disable_web_page_preview=False,
+            )
 
         # ensure no spinny white circle
         context.bot.answer_callback_query(query.id)
